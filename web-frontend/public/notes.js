6c953bab61615374a19f6927660fa74c25e8bb2b
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2016-11-15 12:30:05 -0600

    start form building

--- web-frontend/public/notes.js
+++ web-frontend/public/notes.js
@@ -3,8 +3,35 @@
  */
 console.log("page is up and running");
 
-let button = document.getElementById("saveNotesButton");
-console.log(button);
-button.onclick = function(){
-   console.log("Button clicked");
-};
+window.onload = function(){
+    // prevents default behavior
+   $('#notesTextArea').on('keydown', function(e) {
+       console.log("mouse up");
+       console.log(e.which);
+      if(e.which == 9) {
+          e.preventDefault();
+          e.preventDefault();
+          var start = $(this).get(0).selectionStart;
+          var end = $(this).get(0).selectionEnd;
+
+          // set textarea value to: text before caret + tab + text after caret
+          $(this).val($(this).val().substring(0, start)
+              + "\t"
+              + $(this).val().substring(end));
+
+          // put caret at right position again
+          $(this).get(0).selectionStart =
+              $(this).get(0).selectionEnd = start + 1;
+
+      }
+   });
+   let button = document.getElementById("save");
+   console.log(button);
+   button.onclick = function(){
+
+      let elem = document.getElementById("notesTextArea");
+      console.log(elem);
+      let string = elem.value;
+      console.log(string);
+   };
+}
