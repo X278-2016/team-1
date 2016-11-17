e9b1bd4652caa9e3dd4462f7374f7472b8a31b5e
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2016-11-17 11:30:24 -0600

    Form complete frontend

--- web-frontend/public/notes.js
+++ web-frontend/public/notes.js
@@ -5,33 +5,12 @@ console.log("page is up and running");
 
 window.onload = function(){
     // prevents default behavior
-   $('#notesTextArea').on('keydown', function(e) {
-       console.log("mouse up");
-       console.log(e.which);
-      if(e.which == 9) {
-          e.preventDefault();
-          e.preventDefault();
-          var start = $(this).get(0).selectionStart;
-          var end = $(this).get(0).selectionEnd;
+   let save = document.getElementById("savebutton");
+    $("#savebutton").click( function(e){
+        e.preventDefault();
+        console.log("clicked save button");
+        let obj={};
+        console.log($('#bodypart').val());
+    })
 
-          // set textarea value to: text before caret + tab + text after caret
-          $(this).val($(this).val().substring(0, start)
-              + "\t"
-              + $(this).val().substring(end));
-
-          // put caret at right position again
-          $(this).get(0).selectionStart =
-              $(this).get(0).selectionEnd = start + 1;
-
-      }
-   });
-   let button = document.getElementById("save");
-   console.log(button);
-   button.onclick = function(){
-
-      let elem = document.getElementById("notesTextArea");
-      console.log(elem);
-      let string = elem.value;
-      console.log(string);
-   };
 }
