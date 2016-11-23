2790e3a47b25f1bf7f2a72fde1b923047495f9a8
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2016-11-22 19:16:07 -0600

    inital server route

--- web-frontend/src/server/index.js
+++ web-frontend/src/server/index.js
@@ -26,6 +26,31 @@ app.use(express.static('../../public'));
 
 console.log("server is up and running");
 
+let symptom = [];
+// Handle POST to create a new symptom
+app.post('/v1/bodypart', function(req, res) {
+    let data = req.body;
+    if (!data ||
+        !data.bodypart ||
+        !data.symptom ||
+        !data.description ||
+        !data.severity) {
+        res.status(400).send({ error: 'all form fields required' });
+    } else {
+        let newSymptom = _.pick(data, 'bodypart', 'symptom', 'description', 'severity');
+        newSymptom = _.extend(newSymptom, {
+            id: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5),
+            //duration: Math.floor(Math.random() * 500),
+            //winner: '',
+            //points: Math.floor(Math.random() * 100)
+        });
+        symptom.push(newSymptom);
+        res.status(201).send({
+            symptomID: newSymptom.id
+        });
+    }
+});
+
 
 let server = app.listen(8080, function () {
     console.log('Example app listening on ' + server.address().port);
